b3b81b5dfcb63d55d565a80222fb5cec7adb833f
Omnath Shinde <omnathshinde1@gmail.com>
2026-05-04 05:39:37 +0530

    Add tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
-import "src/App.scss";
-
 function App() {
 	const api = import.meta.env.VITE_API_URL;
 	const env = import.meta.env.VITE_ENV;
 
 	return (
-		<>
-			<h1>Hello World</h1>
+		<div className="h-screen flex flex-col items-center justify-center">
+			<h1 className="bg-red-100">Hello World</h1>
 			<p>API - {api}</p>
 			<p>env - {env}</p>
-		</>
+		</div>
 	);
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "src/index.scss";
+import "./tailwind.scss";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
